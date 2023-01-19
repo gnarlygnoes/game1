663da69e5abe28c9bf46db5868925f44a45a71e0
Toby <sugto555@gmail.com>
2023-01-20 11:57:59 +1300

    Get basics of a conversation system working.

--- src/hud/converse/converse.ts
+++ src/hud/converse/converse.ts
@@ -1,11 +1,41 @@
-import {$Component, Button, Div} from '../../../fiend-ui/src'
+import {$Component, Button, c, Div} from '../../../fiend-ui/src'
 import './converse.css'
+
+interface Prompt {
+  npcText: string
+  playerResponses: PlayerReponse[]
+}
+
+interface PlayerReponse {
+  text: string
+  next: number
+}
 
 export class Converse extends $Component {
   $showingConverse = false
+  $currentPrompt = 0
+
+  $prompt: Prompt[] = [
+    {
+      npcText: '0: Are you feeling lucky punk?',
+      playerResponses: [
+        {text: 'yes', next: 1},
+        {text: 'no', next: 1},
+        {text: 'leave me alone', next: 0},
+      ],
+    },
+    {
+      npcText: 'That will do little good for you',
+      playerResponses: [
+        {text: 'yes', next: 0},
+        {text: 'no', next: 0},
+        {text: 'leave me alone', next: 0},
+      ],
+    },
+  ]
 
   render() {
-    const {$showingConverse} = this
+    const {$showingConverse, $prompt} = this
 
     return Div({
       className: 'Converse',
@@ -15,7 +45,35 @@ export class Converse extends $Component {
           className: 'Converse-showButton',
           onclick: this.onClickShow,
         }),
+
+        $showingConverse
+          ? this.drawConverseControls($prompt[this.$currentPrompt])
+          : null,
       ],
+    })
+  }
+
+  drawConverseControls(prompt: Prompt) {
+    return Div({
+      className: 'Converse-controls',
+      children: [
+        Div({
+          children: [prompt.npcText],
+        }),
+        Div({
+          className: c`Converse-options`,
+          children: prompt.playerResponses.map(this.drawOption),
+        }),
+      ],
+    })
+  }
+
+  drawOption = (playerResponse: PlayerReponse) => {
+    return Button({
+      children: [playerResponse.text],
+      onclick: () => {
+        this.$currentPrompt = playerResponse.next
+      },
     })
   }
 
