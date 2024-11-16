28a755ffe2dfdf22adc47bf8d75cce906886bda6
Toby <tjsuggate@gmail.com>
2024-11-16 18:54:38 +1300

    Update parser lib with learnings from other versions in different languages.

--- src/lib/parser/parsers.test.ts
+++ src/lib/parser/parsers.test.ts
@@ -1,4 +1,16 @@
-import {and, char, number, parse, takeWhile, until, until2, untilLineEnd} from './parsers'
+import {
+  and,
+  char,
+  int,
+  number,
+  parse,
+  takeCharWhile,
+  uint,
+  untilLineEnd,
+  untilS,
+  word,
+} from './parsers'
+import {Input} from './input'
 
 describe('test built in parsers', () => {
   it('untilLineEnd', () => {
@@ -22,14 +34,67 @@ describe('parse2', () => {
   })
 })
 
-describe('takeWhile', () => {
+describe('uint', () => {
+  test('123', () => {
+    expect(parse(uint, '123')).toEqual('123')
+  })
+  test('1009', () => {
+    expect(parse(uint, '1009')).toEqual('1009')
+  })
+  test('0120', () => {
+    expect(parse(uint, '0120abc', {partial: true})).toEqual('0120')
+  })
+})
+
+describe('int', () => {
+  test('123', () => {
+    expect(parse(int, '123')).toEqual('123')
+  })
+  test('0120', () => {
+    expect(parse(int, '0120abc', {partial: true})).toEqual('0120')
+  })
+  test('-123', () => {
+    expect(parse(int, '-123')).toEqual('-123')
+  })
+  test('-0120', () => {
+    expect(parse(int, '-0120abc', {partial: true})).toEqual('-0120')
+  })
+})
+
+describe(untilS.name, () => {
+  const input = new Input('abcdefghijklmnomg')
+  const res = untilS('omg')(input)
+  expect(res).toBeTruthy()
+  expect(res).toEqual('abcdefghijklmn')
+  expect(input.endOfInput()).toBeTruthy()
+})
+
+describe(and.name, () => {
+  test('', () => {
+    const out = parse(and(word('aaa'), word('baa')), 'aaabaa')
+    expect(out).toBeTruthy()
+  })
+})
+
+describe(takeCharWhile.name, () => {
+  test('aaaax', () => {
+    const out = parse(
+      takeCharWhile(c => c !== 'x'),
+      'aaaax',
+      {partial: true},
+    )
+    expect(out).toBeTruthy()
+    expect(out).toEqual('aaaa')
+  })
+
   it('asdf fdf \n', () => {
     const out = parse(
       and(
-        takeWhile(c => c !== '\n'),
-        char('\n')
+        takeCharWhile(c => c !== '\n'),
+        char('\n'),
       ),
-      'asdf fdf' + ' \n'
+      'asdf fdf \n',
+      {partial: true},
     )
 
     expect(out).toEqual(['asdf fdf ', '\n'])
@@ -39,7 +104,7 @@ describe('takeWhile', () => {
 describe('until2', () => {
   const a = 'abc.'
   test(a, () => {
-    const res = parse(until2('.'), a)
+    const res = parse(untilS('.'), a)
 
     expect(res).toEqual('abc')
   })
--- src/lib/parser/parsers.ts
+++ src/lib/parser/parsers.ts
@@ -4,46 +4,30 @@ import {isString} from './util'
 
 // Parses spaces, tabs and line endings.
 export const ___: Parser<''> = optionalWhiteSpace()
-// export const ___: Parser<''> = regex(/[\s]*/).map(() => '' as '')
 
-// 0 or more spaces and tabs.
-export const __: Parser<''> = regex(/[ \t]*/).map(() => '' as const)
-
+/** @deprecated */
 export const number: Parser<string> = regex(/-?(\d+(\.\d+)?)/)
 
-export const int: Parser<string> = regex(/^[-+]?\d+/)
+export const uint: Parser<string> = takeCharWhile(c => isDigit(c))
 
-export const anyWord: Parser<string> = regex(/\w+/)
+export const int = map(and(or(char('-'), optionalWhiteSpace()), uint), res => {
+  return res[0] + res[1]
+})
 
-export const stringLiteral: Parser<string> = regex(/"([^"\\]|\\.)*"/)
+export const untilLineEnd = takeCharWhile(c => !isLineEnd(c))
 
-export const ident: Parser<string> = regex(
-  /^[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*/,
-)
-
-export const lineEnd: Parser<string> = regex(/(\r\n|[\r\n])/).map(() => '')
-
-export const untilLineEnd: Parser<string> = and(
-  regex(/.*/),
-  regex(/(\r\n|[\n\r])?/),
-).map(res => res[0])
+function isLineEnd(val: string): boolean {
+  return val === '\n' || val === '\r\n' || val == '\r'
+}
 
 function isWhiteSpace(val: string): boolean {
   return val.trim() === ''
   // return val === ' ' || val === '\n' || val === '\r' || val === '\t'
 }
 
-export function char2<T extends string>(c: T): Parser<T> {
-  return input => {
-    const r = input.nextChar()
-    let result: T | null = null
-
-    if (r === c) {
-      input.advance()
-      result = c
-    }
-    return result
-  }
+function isDigit(char: string): boolean {
+  const code = char.charCodeAt(0)
+  return code >= 48 && code <= 57 // ASCII codes for '0' to '9'
 }
 
 export function char<T extends string>(c: T): Parser<T> {
@@ -116,60 +100,6 @@ export function regex(re: RegExp): Parser<string> {
   }
 }
 
-// export function or<A>(a: P2<A>): Parser<A>
-// export function or<A, B>(a: P2<A>, b: P2<B>): Parser<A | B>
-// export function or<A, B, C>(a: P2<A>, b: P2<B>, c: P2<C>): Parser<A | B | C>
-// export function or<A, B, C, D>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-// ): Parser<A | B | C | D>
-// export function or<A, B, C, D, E>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-// ): Parser<A | B | C | D | E>
-// export function or<A, B, C, D, E, F>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-// ): Parser<A | B | C | D | E | F>
-// export function or<A, B, C, D, E, F, G>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-// ): Parser<A | B | C | D | E | F | G>
-// export function or<A, B, C, D, E, F, G, H>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-// ): Parser<A | B | C | D | E | F | G | H>
-// export function or<A, B, C, D, E, F, G, H, I>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-// ): Parser<A | B | C | D | E | F | G | H | I>
 export function or<T extends any[]>(...parsers: T): Parser<T> {
   return input => {
     for (const p of parsers) {
@@ -188,209 +118,21 @@ export function and<T extends any[]>(
   ...parsers: {[K in keyof T]: Parser<T[K]>}
 ): Parser<T> {
   return input => {
-    const startPos = input.getPosition()
-    let failed = false
+    const startPos = input.position
+    const results = []
 
-    // TODO: Why the map?
-    const results = parsers.map(p => {
-      if (!failed) {
-        const result = p(input)
-
-        if (result === null) {
-          input.setPosition(startPos)
-          failed = true
-        } else return result
+    for (const parser of parsers) {
+      const result = parser(input)
+      if (result === null) {
+        input.setPosition(startPos)
+        return null
       }
-      return undefined
-    })
-
-    if (failed) return null
+      results.push(result)
+    }
 
     return results as T
   }
 }
-
-// export function and<A>(a: P2<A>): Parser<[A]>
-// export function and<A, B>(a: P2<A>, b: P2<B>): Parser<[A, B]>
-// export function and<A, B, C>(a: P2<A>, b: P2<B>, c: P2<C>): Parser<[A, B, C]>
-// export function and<A, B, C, D>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-// ): Parser<[A, B, C, D]>
-// export function and<A, B, C, D, E>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-// ): Parser<[A, B, C, D, E]>
-// export function and<A, B, C, D, E, F>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-// ): Parser<[A, B, C, D, E, F]>
-// export function and<A, B, C, D, E, F, G>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-// ): Parser<[A, B, C, D, E, F, G]>
-// export function and<A, B, C, D, E, F, G, H>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-// ): Parser<[A, B, C, D, E, F, G, H]>
-// export function and<A, B, C, D, E, F, G, H, I>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-// ): Parser<[A, B, C, D, E, F, G, H, I]>
-// export function and<A, B, C, D, E, F, G, H, I, J>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-//   j: P2<J>,
-// ): Parser<[A, B, C, D, E, F, G, H, I, J]>
-// export function and<A, B, C, D, E, F, G, H, I, J, K>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-//   j: P2<J>,
-//   k: P2<K>,
-// ): Parser<[A, B, C, D, E, F, G, H, I, J, K]>
-// export function and<A, B, C, D, E, F, G, H, I, J, K, L>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-//   j: P2<J>,
-//   k: P2<K>,
-//   l: P2<L>,
-// ): Parser<[A, B, C, D, E, F, G, H, I, J, K, L]>
-// export function and<A, B, C, D, E, F, G, H, I, J, K, L, M>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-//   j: P2<J>,
-//   k: P2<K>,
-//   l: P2<L>,
-//   m: P2<M>,
-// ): Parser<[A, B, C, D, E, F, G, H, I, J, K, L, M]>
-// export function and<A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-//   j: P2<J>,
-//   k: P2<K>,
-//   l: P2<L>,
-//   m: P2<M>,
-//   n: P2<N>,
-// ): Parser<[A, B, C, D, E, F, G, H, I, J, K, L, M, N]>
-// export function and<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-//   j: P2<J>,
-//   k: P2<K>,
-//   l: P2<L>,
-//   m: P2<M>,
-//   n: P2<N>,
-//   o: P2<O>,
-// ): Parser<[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]>
-// export function and<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
-//   a: P2<A>,
-//   b: P2<B>,
-//   c: P2<C>,
-//   d: P2<D>,
-//   e: P2<E>,
-//   f: P2<F>,
-//   g: P2<G>,
-//   h: P2<H>,
-//   i: P2<I>,
-//   j: P2<J>,
-//   k: P2<K>,
-//   l: P2<L>,
-//   m: P2<M>,
-//   n: P2<N>,
-//   o: P2<O>,
-//   p: P2<P>,
-// ): Parser<[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P]>
-// export function and<T extends any[]>(...parsers: T) {
-//   return mkParser(input => {
-//     const startPos = input.getPosition()
-//     let failed = false
-//
-//     const results = parsers.map(p => {
-//       if (!failed) {
-//         const result = applyParser(p, input)
-//
-//         if (result === null) {
-//           input.setPosition(startPos)
-//           failed = true
-//         } else return result
-//       }
-//       return undefined
-//     })
-//
-//     if (failed) return null
-//
-//     return results
-//   })
-// }
 
 // Doesn't advance position
 // TODO: Make this not polymorphic?
@@ -400,7 +142,7 @@ export function not<T>(parserIn: string | Parser<T>): Parser<string> {
   ) as Parser<T>
 
   return input => {
-    const pos = input.getPosition()
+    const pos = input.position
     const result = parser(input)
 
     if (result !== null) {
@@ -489,51 +231,38 @@ export function repParserSep<T, U>(
   }
 }
 
-export function takeWhile(condition: (c: string) => boolean): Parser<string> {
+export function takeCharWhile(
+  condition: (c: string) => boolean,
+): Parser<string> {
   return input => {
-    let nc = input.nextChar()
-    const chars: string[] = []
+    const {code, position, len} = input
 
-    while (nc !== undefined && condition(nc)) {
-      chars.push(nc)
-      input.advance()
-      nc = input.nextChar()
+    let i
+    for (i = position; i < len; i++) {
+      const c = code[i]
+      if (!condition(c)) {
+        if (i === position) return null
+        break
+      }
     }
-
-    if (chars.length === 0) return null
-
-    return chars.join('')
+    input.setPosition(i)
+    return code.slice(position, i)
   }
 }
 
-// // Parses any string until untilStr. Discards untilStr from result.
-// export function until(untilStr: string): Parser<string> {
-//   return and(regex(RegExp(`[\\s\\S]*?(?=${untilStr})`)), word(untilStr)).map(
-//     res => {
-//       return res[0]
-//     },
-//   )
-// }
-
+// Input is consumed including str, but str is not included in the result.
 export function untilS(str: string): Parser<string> {
-  return (i: Input) => {
+  return (input: Input) => {
     const strLen = str.length
-    const end = i.len - strLen
+    const startPos = input.position
 
-    // for i.p
+    const i = input.code.indexOf(str, input.position)
+    if (i > 0) {
+      input.setPosition(i + strLen)
+      return input.code.slice(startPos, i)
+    }
+    return null
   }
-}
-
-// Parses any string until untilStr. Discards untilStr from result.
-// TODO: This is until char, not string?
-export function until2(untilStr: string): Parser<string> {
-  return map(
-    and(
-      takeWhile(c => c !== untilStr),
-      word(untilStr),
-    ),
-    res => res[0],
-  )
 }
 
 export function map<T, U>(parser: Parser<T>, f: (result: T) => U): Parser<U> {
